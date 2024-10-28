684e7209579f630fff6f3cf9f27f5c397f26315f
Alexandre WEISSER <alexandre.weisser@gmail.com>
2024-10-28 21:23:01 +0100

    better data

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from PyDMXControl.controllers import OpenDMXController
-from PyDMXControl.profiles.Generic import RGB_Vdim
 from StairvilleFixture import StairvilleFixture
+from openpyxl import Workbook
+
 
 dmx = OpenDMXController()
 fixture = dmx.add_fixture(StairvilleFixture, name="StairvilleFixture")
 dmx.web_control()
 
+# Once the console debug mode is exited the script will continue, to stop it
+#  exiting and stopping DMX output when can use a built-in sleep function.
+# This sleep function will wait until enter is pressed in the console before continuing.
+dmx.sleep_till_enter()
 
 dmx.close()
