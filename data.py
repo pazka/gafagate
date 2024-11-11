f512c4210a55c2695d13f159ec742b81eeea22a8
Alexandre WEISSER <alexandre.weisser@gmail.com>
2024-11-11 20:05:56 +0100

    inflation

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ from fixtures.ChauvetFixture import ChauvetFixture
 from data_process.data_revenue import RevenueData
 from data_process.data_fines import FinesData
 from helpers import normalize,interpolate
+from data_process.data_inflation import InflationData
 
 dmx = OpenDMXController()
 
@@ -27,23 +28,26 @@ else:
 wb = load_workbook(filename="data/data.xlsx", read_only=True, data_only=True)
 revenue_data = RevenueData(wb)
 fines_data = FinesData(wb)
+inflation_data = InflationData(wb)
+wb.close()
 
 fixture1.dim(255, 1/200)
 fixture2.dim(255, 1)
 
-dim_speed = 0.05
-color_transition_time = 0.5
+dim_speed = 0.01
+color_transition_time = 1
 
 while True:
     prev_color = (0, 0, 0)
     for i in range(0, len(revenue_data)):
         revenue_data_point = revenue_data[i]
         fines_data_point = fines_data[i]
+        inflation_data_point = inflation_data[i]
 
         new_color_uv = (
             revenue_data.to_uv(revenue_data_point) * 255,
             fines_data.to_uv(fines_data_point) * 255,
-            0
+            inflation_data.to_uv(inflation_data_point) * 255
         )
 
         print("-------------------------------")
@@ -51,8 +55,8 @@ while True:
         print("GAFAM Revenue (millions) : ", round(
             revenue_data_point[1]))
         print("GDPR Fines (millions): ", round(
-            fines_data_point[1]))
-        print("TBD Inflation Blue value : ", new_color_uv[2])
+            fines_data_point[1]/1000000))
+        print("OECD members Rate %: ", round(inflation_data_point[1], 2))
         print("Color (R,G,B): ", (round(new_color_uv[0]), round(new_color_uv[1]), round(new_color_uv[2])))
         # display next color gradually
         counter = 0
@@ -60,7 +64,7 @@ while True:
             counter += dim_speed
             # TODO : fix : interpoation sometimes give negative values
             transition_color = interpolate(prev_color, new_color_uv, counter / color_transition_time)
-            print("Transition Color (R,G,B): ", (round(transition_color[0]), round(transition_color[1]), round(transition_color[2])))
+            #print("Transition Color (R,G,B): ", (round(transition_color[0]), round(transition_color[1]), round(transition_color[2])))
             fixture1.simple_color(transition_color)
             fixture2.simple_color(transition_color)
             sleep(dim_speed)
--- data_process/data_inflation.py
+++ data_process/data_inflation.py
@@ -5,29 +5,35 @@ from openpyxl.cell import Cell
 from data_process.data_helper import DataHelper, DataDate, DataPoint
 
 
-class RevenueData(DataHelper):
+class InflationData(DataHelper):
     def __init__(self, wb: Workbook):
-        sheet = wb["Revenue"]
+        sheet = wb["Inflation"]
         # yearly data from 2011 to 2023
-        raw_data: list[list[Cell]] = sheet["G2:G14"]
+        raw_data: list[list[Cell]] = sheet["B3:B16"]
         yearly_data: list[DataPoint] = []
 
         for i, data in enumerate(raw_data):
             if type(data[0].value) == str:
-                value = float(data[0].value.replace(",", ""))
+                value = float(data[0].value.replace("%", ""))
             else:
                 value = data[0].value
-
-            yearly_data.append((date(2011+i, 1, 1), value))
+            value = value * 100
+            yearly_data.append((date(2010+i, 1, 1), value))
 
         monthly_data: list[DataPoint] = []
-        for year_data in yearly_data:
+        for i in range(0, len(yearly_data)-1):
+            year_data = yearly_data[i]
+            next_year_data = yearly_data[i+1]
+
             year = year_data[0].year
-            revenue = year_data[1]
-            month_revenue = revenue/12
+            inflation = year_data[1]
+            next_inflation = next_year_data[1]
+
+            month_inflation = (next_inflation-inflation)/12
 
             for j in range(1, 13):
-                monthly_data.append((date(year, j, 1), month_revenue * j))
+                monthly_data.append(
+                    (date(year, j, 1), inflation+(month_inflation * j)))
 
         super().__init__(monthly_data)
         self.filter_dates(date(2019, 1, 1), date(2023, 12, 31))
